5c9383bb2b9c649abfc991ac7f43b37d873c9b42
Aubrey Howell <simplyaubs@gmail.com>
2014-04-06 12:32:55 -0600

    Set up initial gemfile and lib/spec directories

---
+++ lib/recipe_converter.rb
@@ -0,0 +1,9 @@
+class RecipeConverter
+  def to_tablespoon
+  # [{}{}]
+  end
+
+  def to_cups
+  # [{}{}]
+  end
+end
---
+++ spec/recipe_converter_spec.rb
@@ -0,0 +1,7 @@
+require 'recipe_converter'
+
+describe RecipeConverter do
+  it "can convert recipes" do
+    expect(true).to eq true
+  end
+end
