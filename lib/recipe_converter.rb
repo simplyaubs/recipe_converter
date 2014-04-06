13ff8df2702adfdfa117ad2b9f71d3b5d3884d32
Aubrey Howell <simplyaubs@gmail.com>
2014-04-06 17:16:34 -0600

    Can convert grams to cups

--- lib/recipe_converter.rb
+++ lib/recipe_converter.rb
@@ -2,7 +2,24 @@ class RecipeConverter
   def initialize(ingredients)
     @ingredients = ingredients
   end
+
   def to_tablespoons
-    @ingredients
+    fill(15)
+  end
+
+  def to_cups
+    fill(240)
+  end
+
+  private
+  def fill(x)
+    my_array = []
+    @ingredients.each do |item|
+      item.each do |ingredient, grams|
+        my_array << {ingredient => grams/x}
+      end
+    end
+    my_array
   end
 end
+
--- spec/recipe_converter_spec.rb
+++ spec/recipe_converter_spec.rb
@@ -1,10 +1,17 @@
 require 'rspec'
 require 'recipe_converter'
 
-describe RecipeConverter do
-  it "prints an array of ingredients" do
-    ingredients = RecipeConverter.new([:salt => 45, :sugar => 60])
+describe "Recipe Converter" do
+  it "Converts grams to tablespoons" do
+    ingredients = RecipeConverter.new(["sugar" => 45, "salt" => 60])
 
-    expect(ingredients.to_tablespoons).to eq([:salt => 45, :sugar => 60])
+    expect(ingredients.to_tablespoons).to eq([{"sugar" => 3}, {"salt" => 4}])
+  end
+
+  it "Converts grams to cups" do
+    ingredients = RecipeConverter.new(["sugar" => 240, "salt" => 480])
+
+    expect(ingredients.to_cups).to eq([{"sugar" => 1}, {"salt" => 2}])
   end
 end
+
